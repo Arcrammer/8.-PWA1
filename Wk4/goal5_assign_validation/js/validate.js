d29fab07556430230d3c13789f7b660c13128da1
Arcrammer <Arcrammer@users.noreply.github.com>
2014-10-22 16:22:39 -0400

    Couldn't fix the bug -- No idea
    I have completed the assignment but for some reason the document isn’t
    updating upon submission. I don’t understand why but I’m submitting
    this as it is because I’ve done everything and there’s nothing I can do
    to fix it. I have no idea why this is happening. I’m a bad developer :(

--- Wk4/goal5_assign_validation/js/validate.js
+++ Wk4/goal5_assign_validation/js/validate.js
@@ -8,41 +8,44 @@
     Practice! Form Validation  */
 
 (function(){
-    myform.onsubmit = function(e) { // I have no idea what the event is passing to this function
+	var mainForm = document.getElementById("mainForm");
+    mainForm.onsubmit = function(e) { // I have no idea what the event is passing to this function
 
         //Below is one example of the validateField call with an argument.
         //You must dynamically retrieve the ID name from the DOM/HTML.
 
-        // Dynamically fetching the value of the 'name' attrubutes based on DOM tree then passing them as a parameter to the function 'validateField()' defined after 'myform.onsubmit()'
-        validateField(myform.childNodes[1].childNodes[1].childNodes[3].name); // Executes all of the code after this block
-        validateField(myform.childNodes[1].childNodes[3].childNodes[3].name); // Executes all of the code after this block
-        validateField(myform.childNodes[1].childNodes[5].childNodes[3].name); // Executes all of the code after this block
-        validateField(myform.childNodes[1].childNodes[7].childNodes[3].name); // Executes all of the code after this block
-        validateField(myform.childNodes[1].childNodes[9].childNodes[3].name); // Executes all of the code after this block
+        // Dynamically fetching the value of the 'name' attrubutes based on DOM tree then passing them as a parameter to the function 'validateField()' defined after 'mainForm.onsubmit()'
+        validateField(mainForm.childNodes[1].childNodes[1].childNodes[3].name); // Executes all of the code after this block
+        validateField(mainForm.childNodes[1].childNodes[3].childNodes[3].name); // Executes all of the code after this block
+        validateField(mainForm.childNodes[1].childNodes[5].childNodes[3].name); // Executes all of the code after this block
+        validateField(mainForm.childNodes[1].childNodes[7].childNodes[3].name); // Executes all of the code after this block
+        validateField(mainForm.childNodes[1].childNodes[9].childNodes[3].name); // Executes all of the code after this block
 
         e.preventDefault();
         return false;
     };
 
-    var validateField = function(inputName){
+    var validateField = function(inputName) {
 
-	    // Wouldn't a switch() be more efficient here?
-        if (inputName.name === 'f_username'){
-            var pattern = /^[A-Z]\w*[\s*[A-Z]\w*]*/; // Regular expression to compare to the input provided for 'f_username'
-
-            //You will need to create an else-if statement for each input field id.  The
-            //      format will be similar to the above IF statement.
-        } else if(inputName.name === 'f_email') {
-        	var pattern = /^\w*\@\w*\.\w{2,4}/; // Regular expression to compare to the input provided for 'f_email'
-        } else if(inputName.name === 'f_phone') {
-        	var pattern = /\(\d{3,3}\)\d{3,3}\-\d{4,4}/; // Regular expression to compare to the input provided for 'f_phone'
-        } else if(inputName.name === 'f_ssn') {
-        	var pattern = /\d{3,3}\-\d{2,2}\-\d{4,4}/; // Regular expression to compare to the input provided for 'f_ssn'
-        } else if(inputName.name === 'f_password') {
-        	var pattern = /^\w{4,15}/; // Regular expression to compare to the input provided for 'f_password'
+        // Wouldn't a switch() be more efficient here?
+        if (inputName === 'f_username'){
+            pattern = /^[A-Z][a-zA-Z]*\s[A-Z][a-zA-Z]*/; // Regular expression to compare to the input provided for 'f_username'
+        } else if(inputName === 'f_email') {
+        	pattern = /^\w*\@\w*\.\w{2,4}/; // Regular expression to compare to the input provided for 'f_email'
+        } else if(inputName === 'f_phone') {
+        	pattern = /\(\d{3}\)\d{3}\-\d{4}/; // Regular expression to compare to the input provided for 'f_phone'
+        } else if(inputName === 'f_ssn') {
+        	pattern = /\d{3}\-\d{2}\-\d{4}/; // Regular expression to compare to the input provided for 'f_ssn'
+        } else if(inputName === 'f_password') {
+        	pattern = /^\w{4,15}$/; // Regular expression to compare to the input provided for 'f_password'
+        } else {
+        	console.log("Something went tragically wrong! (One of the elements likely doesn't have an acceptable name attribute value.)");
+        	pattern = null;
         }
 
-    var pass = pattern.test(inputName.value);
+    // By this point, the variable 'pattern' should have a value and exist. Now we'll use its' .test() method passing the input value
+
+    var pass = pattern.test(inputName.value); // ...then assign the returned Boolean result to the variable 'pass'
     var errorMsg = inputName.nextSibling.nextSibling.nextSibling.nextSibling;
 
     if (!pass || inputName.value.length < 2){
@@ -51,13 +54,7 @@
         } else if (pass && inputName.value.length > 5){
             errorMsg.style.display='none';
             inputName.style.backgroundColor = 'green';
-        } else {
-            errorMsg.style.display='none';
-            inputName.style.backgroundColor = 'yello';
-        };
+        }
     };
 
 })();  // end wrapper
-
-
-
